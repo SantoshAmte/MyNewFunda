dbdbdf1ecceef4b689c4b8f53e4d0bfd60069769
shivg <shivg@DESKTOP-1Q4D7OF>
2022-06-17 14:30:24 +0530

    Add project files.

---
+++ MyNewFunda/Basic1/EvenOrOdd.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyNewFunda.Basic1
+{
+    class EvenOrOdd
+    {
+        static void Main(string[] args)
+        {
+            int num = 18;
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("num is even ");
+            }
+            else
+            {
+                Console.WriteLine("num is odd");
+            }
+            Console.ReadLine();
+        }
+
+
+
+    }
+    
+    class evenorodd2
+    {
+        static void Main(string[] args)
+        {
+            for (int i=0; i<= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
